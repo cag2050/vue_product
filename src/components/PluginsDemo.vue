f99f1d4079b61fa03d14a2cc850fad2c2ca11725
cag2050 <cag2050@163.com>
2017-10-30 18:39:21 +0800

    m

--- src/components/PluginsDemo.vue
+++ src/components/PluginsDemo.vue
@@ -4,10 +4,13 @@ div
     ol
         li
             p 全局 Vue.directive 测试(input框默认获得焦点):
-            input(class='b' v-myfocus='true')
+            input(v-myfocus='true')
         li
             p 全局 Vue.filter 测试:
             p {{ number | vcntFormat }}
+        li
+            p 全局 mixin的directive 测试（点击外面:
+            p(:class="bgColor === 'yellow' ? 'bgYellow' : 'bgBlue'" v-clickoutside='clickOutside') 点击此区域外，此区域背景颜色会交替变换黄、蓝色
 </template>
 
 <script>
@@ -15,18 +18,23 @@ export default {
     name: '',
     data () {
         return {
-            number: 120000
+            number: 120000,
+            bgColor: 'yellow'
         }
     },
     mounted () {
-        console.log('mixin中全局data数据的测试：' + this.someValue)
-        console.log('Vue.prototype.getDate 全局函数的测试：' + this.getDate())
+        console.log('mixin中全局data数据 的测试：' + this.someValue1)
+        console.log('Vue.prototype上的值 的测试：' + this.someValue2)
+        console.log('Vue.prototype.getDate 全局函数 的测试：' + this.getDate())
     },
     computed: {
 
     },
     methods: {
-
+        clickOutside () {
+            console.log('clickOutside 事件触发')
+            this.bgColor === 'yellow' ? this.bgColor = 'blue' : this.bgColor = 'yellow'
+        }
     }
 }
 </script>
@@ -36,4 +44,8 @@ div
     ol
         li
             text-align left
+            .bgYellow
+                background-color yellow
+            .bgBlue
+                background-color blue
 </style>
--- src/pluginsUtil/index.js
+++ src/pluginsUtil/index.js
@@ -2,16 +2,19 @@ import Clickoutside from 'element-ui/lib/utils/clickoutside'
 
 export default {
     install (Vue, options) {
+        // 全局注册混合对象后，会影响到 所有 之后创建的 Vue 实例。
+        // 谨慎使用
         Vue.mixin({
             directives: {
                 Clickoutside
             },
             data () {
                 return {
-                    someValue: 'some value'
+                    someValue1: 'some value1：mixin的data里的值'
                 }
             }
         })
+        Vue.prototype.someValue2 = 'someValue2：Vue.prototype上的值'
         Vue.prototype.getDate = function () {
             let dateNew = new Date()
             return dateNew
